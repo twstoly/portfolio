5889be51f8ba292a8a3fd49c5e2a43a6e0767317
Oliver <oliver.mwangale@moringaschool.com>
2021-11-07 12:50:23 +0300

    update js file - adding animation, toggle-menu, slide-up.

--- script.js
+++ script.js
@@ -14,4 +14,38 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["an Enterprenuer", "a SoftWare Developer", "an Android Developer", "an Tech-Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["an Enterprenuer", "a SoftWare Developer", "an Android Developer", "an Tech-Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
 });
